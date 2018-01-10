0714e19b179cbc89d85a4bdee04d55827ebfd652
Alex Smith <alex_smith3@hotmail.com>
2018-01-10 10:52:38 +0000

    added game, board, squares

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Game from "./components/game";
+
+class App extends Component {
+  render() {
+    return (
+      <Game/>
+    );
+  }
+}
+
+export default App;
